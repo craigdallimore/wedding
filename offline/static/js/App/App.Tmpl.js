9ea1b44813f2c060b0087c7fcee58e67027f79b5
Craig <decoy9697@gmail.com>
2013-09-08 13:37:45 +0100

    Remove the old ways

--- offline/static/js/App/App.Tmpl.js
+++
@@ -1,3 +0,0 @@
-App.module("Tmpl", function(Tmpl, App) {
-    Tmpl.sample=function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape, __d = obj.obj || obj;__p += '<p>This is sample HTML. Kaizen.</p>';return __p};
-});
